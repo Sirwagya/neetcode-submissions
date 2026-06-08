771c5038f05dfb0780ac0435c80d1f67267efefc
Sirwagya Shekhar <sirwagya7@gmail.com>
2026-06-08 17:55:55 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = Integer.MIN_VALUE;
+        int profit = 0;
+        for (int i = prices.length-1; i >= 0; i--){
+            max = Math.max(max, prices[i]);
+            profit = Math.max(profit, max - prices[i]);
+        }
+        return profit;
+        
+    }
+}
